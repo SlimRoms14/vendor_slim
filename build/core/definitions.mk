a02763af60434725d52ed69e553c30b8e1f9d8d8
Bruno Martins <bgcngm@gmail.com>
2023-10-25 00:53:43 +0100

    slim: Move all QCOM board configs to hardware/qcom-caf/common
    Change-Id: I6ead35cf884255a8897a3e3e27fc7936d11be616

--- build/core/definitions.mk
+++ build/core/definitions.mk
@@ -7,6 +7,3 @@ endef
 
 # Include board/platform macros
 include vendor/slim/build/core/utils.mk
-
-# Include vendor platform definitions
-include vendor/slim/build/core/vendor/*.mk
--- config/BoardConfigSlim.mk
+++ config/BoardConfigSlim.mk
@@ -1,7 +1,7 @@
 include vendor/slim/config/BoardConfigKernel.mk
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-include vendor/slim/config/BoardConfigQcom.mk
+include hardware/qcom-caf/common/BoardConfigQcom.mk
 endif
 
 include vendor/slim/config/BoardConfigSoong.mk
